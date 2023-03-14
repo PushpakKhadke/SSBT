5d7561917c340e925e3b075c67f451fdbe322c8f
Pushpak Khadke <100704509+PushpakKhadke@users.noreply.github.com>
2023-03-14 22:27:20 +0530

    Create 5.cpp

---
+++ OOP Praticals/5.cpp
@@ -0,0 +1,27 @@
+// 5. Program For Factorial By Using Copy Constructor
+
+# include <iostream>
+using namespace std;
+class fact{
+    private:
+        int n;
+    public:
+        fact(int a){ // Parameterized Constructor
+            n = a;
+        }
+        fact(fact &b){ // Copy Constructor
+            int f = 1;
+            n = b.n;
+            for(int i = 1;i<b.n;i++)
+                f = f * i;
+            cout<<"\n Factorial = "<<f;
+        } 
+};
+
+int main(){
+    int a;
+    cout<<"\n Enter the Number = ";
+    cin >> a;
+    fact p(a);
+    fact q(p);
+}
